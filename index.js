902d18f4eea2e18a3d950f0eccf38b13a0aa2820
omarfarukee <umarfaruque1920@gmail.com>
2024-03-15 19:59:25 +0600

    single user get done

--- index.js
+++ index.js
@@ -11,12 +11,23 @@ const run = async () => {
        const users = [
         { username: "OmarFaruk", password: "omar123" },
         { username: "RakibulHasan", password: "rakib123" },
+        { username: "Marzia", password: "marzia123" },
+        { username: "linkonvai", password: "linkon123" },
       ];
 
 try {
 
     app.get("/users", (req, res) => {
         res.json(users);
+      });
+      app.get("/users/:username", (req, res) => {
+        const username = req.params.username;
+        const foundUser = users.find((user) => user.username === username);
+        if (foundUser) {
+          res.json(foundUser);
+        } else {
+          res.status(404).json({ message: "User not found" });
+        }
       });
 
 }  finally {
