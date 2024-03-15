286ff22623fd363956b07b33dec298873169eac5
omarfarukee <umarfaruque1920@gmail.com>
2024-03-15 19:54:39 +0600

    All users get from hardcoded array of users

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const cors = require("cors");
 app.use(cors())
   
 app.use(express.json());
+
+const run = async () => {
+       const users = [
+        { username: "OmarFaruk", password: "omar123" },
+        { username: "RakibulHasan", password: "rakib123" },
+      ];
+
+try {
+
+    app.get("/users", (req, res) => {
+        res.json(users);
+      });
+
+}  finally {
+}
+}
+run().catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
     res.send("BitechX Runnig successfully");
 });
